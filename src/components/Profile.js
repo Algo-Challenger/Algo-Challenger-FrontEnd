17b50f665abcd6a31a625b6e7ea7a64254d6d7dc
Xavier Hillman <xhillman13@gmail.com>
2022-09-07 14:56:04 -0400

    add ability to set user profile data

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class Profile extends React.Component {
   render() {
+    let completedChallenges = this.props.profile.challengesInitialized.filter( (challenge, i) => {
+        return challenge.hasCompleted === true;
+    })
+
     return (
-      <h1>{this.props.profile.name}</h1>
+      <>
+        <h1>Hey, {this.props.profile.name}!</h1>
+        <div>
+          <h2>Completed Challenges</h2>
+          <ul>
+          {
+            completedChallenges && completedChallenges.map((challenge, i) => {
+              return <li key={challenge.challengeId}>
+                    <Link to={`/challenge/${challenge.challengeName}`}
+                          style={{ display: "block", margin: "1rem 0" }}>{challenge.challengeName}</Link>
+              </li>
+            })
+          }
+          </ul>
+        </div>
+        <div>
+          <h3>Current Score:</h3>
+          <h3>{this.props.profile.score}</h3>
+        </div>
+      </>
     )
   }
 }
