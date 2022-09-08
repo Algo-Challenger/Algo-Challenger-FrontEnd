810bc66813593fb235ac0e92378f443de37e5bfd
Xavier Hillman <xhillman13@gmail.com>
2022-09-08 18:30:34 -0400

    add styling to profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,27 +7,91 @@ class Profile extends React.Component {
         return challenge.hasCompleted === true;
     })
 
+    const profileStyle = {
+      width: "80%",
+      marginTop: "3rem",
+      marginLeft: "auto",
+      marginRight: "auto",
+      display: "flex",
+      flexDirection: "column"
+    }
+
+    const headerStyle = {
+      marginLeft: "auto",
+      marginRight: "auto",
+      marginTop: "1rem",
+      marginBottom: "2rem",
+      fontSize: "2rem"
+    }
+
+    const bodyStyle = {
+      display: "flex",
+      flexDirection: "column"
+    }
+
+    const challengeList = {
+      background: "rgba( 255, 255, 255, 0.15 )",
+			boxShadow: "0 8px 32px 0 rgba( 200, 200, 220, 0.18 )",
+			backdropFilter: "blur( 5px )",
+			borderRadius: "10px",
+      marginLeft: "auto",
+      marginRight: "auto",
+      display: "flex",
+      flexDirection: "column",
+      padding: "1rem"
+    }
+
+    const scoreBox = {
+      background: "rgba( 255, 255, 255, 0.15 )",
+			boxShadow: "0 8px 32px 0 rgba( 200, 200, 220, 0.18 )",
+			backdropFilter: "blur( 5px )",
+			borderRadius: "10px",
+      marginRight: "auto",
+      marginLeft: "auto",
+      marginBottom: "2rem",
+      display: "flex",
+      padding: ".5rem"
+    }
+
+    const sectionHeader = {
+      fontSize: "1.5rem"
+    }
+
+    const challengeItem = {
+      display: "flex",
+      paddingLeft: "2.7rem",
+      paddingRight: "auto",
+      color: "white",
+      marginTop: "1rem"
+    }
+
+  
+
     return (
-      <>
-        <h1>Hey, {this.props.profile.name}!</h1>
-        <div>
-          <h2>Completed Challenges</h2>
-          <ul>
-          {
-            completedChallenges && completedChallenges.map((challenge, i) => {
-              return <li key={challenge.challengeId}>
-                    <Link to={`/challenge/${challenge.challengeName}`}
-                          style={{ display: "block", margin: "1rem 0" }}>{challenge.challengeName}</Link>
-              </li>
-            })
-          }
-          </ul>
+      <div style={profileStyle}>
+        <h1 style={headerStyle}>Hey, {this.props.profile.name}!</h1>
+        <div style={bodyStyle}>
+          <div style={scoreBox}>
+            <h3 style={sectionHeader}>Current Score: {this.props.profile.score}</h3>
+          </div>
+          <div style={challengeList}>
+            <h2 style={sectionHeader}>Completed Challenges</h2>
+            <ul>
+            {
+              completedChallenges && completedChallenges.map((challenge, i) => {
+                return <li key={challenge.challengeId}>
+                      <Link to={`/challenge/${challenge.challengeName}`}
+                            style={challengeItem}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="limegreen" className="w-5 h-5">
+                            <path fillRule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clipRule="evenodd" />
+                          </svg>
+                          {challenge.challengeName}</Link>
+                </li>
+              })
+            }
+            </ul>
+          </div>
         </div>
-        <div>
-          <h3>Current Score:</h3>
-          <h3>{this.props.profile.score}</h3>
-        </div>
-      </>
+      </div>
     )
   }
 }
--- src/components/ProfileDropdown.js
+++ src/components/ProfileDropdown.js
@@ -6,9 +6,20 @@ class ProfileDropdown extends React.Component
 {
 	render()
 	{
+		const buttonStyle = {
+			background: "#0A0068",
+			padding: ".5rem",
+			borderRadius: "5px"
+		}
+
+		const modalstyle = {
+			borderRadius: "5px",
+		}
+
+
 		return (
-			<div className="absolute bg-blue-50 w-40 h-auto right-0 top-0 flex flex-col items-center p-3 mr-5 mt-5">
-				<Link to="/profile" className="font-semibold mb-3">My Profile</Link>
+			<div className="absolute bg-blue-50 w-40 h-auto right-0 top-0 flex flex-col items-center p-3 mr-5 mt-5" style={modalstyle}>
+				<Link to="/profile" className="font-semibold mb-3" style={buttonStyle}>My Profile</Link>
 				<LogoutButton logout={this.props.logout}/>
 			</div>
 		);
