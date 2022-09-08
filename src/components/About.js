400076fcedf88b81a08d2b3109e1ac1f322bc390
Keelen Fisher <keelencarrera@gmail.com>
2022-09-07 20:03:01 -0500

    adding in CSS Style for About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,23 @@
 import React from "react";
 import Developer from "./Developer";
 
-class About extends React.Component
-{
-	render()
-	{
+class About extends React.Component {
+	render() {
+
+		const aboutStyle = {
+			background: 'linear-gradient(45deg, #0A0068, #26007B, #42008D, black, #5E00A0, #7A00B2)',
+			color: 'blue',
+			boxShadow: 'rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px',
+			fontSize: '1rem',
+
+		}
+
+
 		return (
-			<div className="flex-1 flex flex-col justify-between my-10">
+			<div className="flex-1 flex flex-col justify-between my-10" style={aboutStyle}>
 				<Developer
 					image="1"
-					name="Keelan Fisher"
+					name="Keelen Fisher"
 					description="1"
 				/>
 				<Developer
