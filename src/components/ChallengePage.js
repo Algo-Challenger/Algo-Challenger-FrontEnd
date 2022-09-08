fdeeda86bb364dc4fb1120bac73d40707be83d56
Xavier Hillman <xhillman13@gmail.com>
2022-09-08 16:47:04 -0400

    add styling

--- src/components/ChallengePage.js
+++ src/components/ChallengePage.js
@@ -40,16 +40,42 @@ class ChallengePage extends React.Component
 
 	render()
 	{
+
+		const boxStyle = {
+			background: "rgba( 255, 255, 255, 0.15 )",
+			boxShadow: "0 8px 32px 0 rgba( 200, 200, 220, 0.18 )",
+			backdropFilter: "blur( 5px )",
+			borderRadius: "10px",
+			height: "15rem",
+			marginBottom: "2rem",
+			padding: "1.5rem"
+		}
+
+		const headerStyle = {
+			fontSize: "2rem",
+			marginBottom: "1rem"
+		}
+
+		const buttonStyle = {
+			background: "#287E9B",
+			borderRadius: "10px",
+			border: "none"
+		}
+
+		const edges = {
+			borderRadius: "10px"
+		}
+
 		console.log(this.props.status)
 		return (
 			<div className="px-10 pt-2 flex flex-1 justify-evenly max-h-screen">
 				<div className="flex flex-col w-5/12">
-					<div className=" flex-1 px-3 py-1">
-						<h1 className="text-center font-semibold">{this.props.challenge.name}</h1>
+					<div style={boxStyle}>
+						<h1 className="text-center font-semibold" style={headerStyle}>{this.props.challenge.name}</h1>
 						<p>{this.props.challenge.instructions}</p>
 					</div>
-					<div className=" flex-1 px-3 py-1 mt-3 text-center">
-						<h1 className="text-center font-semibold">Tests</h1>
+					<div style={boxStyle}>
+						<h1 className="text-center font-semibold" style={headerStyle}>Tests</h1>
 						<p>Input: {this.props.challenge.tests.input}</p>
 						<p>Output: {this.props.challenge.tests.output}</p>
 					</div>
@@ -57,15 +83,16 @@ class ChallengePage extends React.Component
 				<div className="w-6/12 flex flex-col h-full justify-around">
 					<form onSubmit={this.handleSubmit} className="text-right flex-1">
 						<textarea
+							style={edges}
 							className="w-full h-5/6 border-4 border-black px-3 py-1 resize-none text-black bg-neutral-200"
 							defaultValue={this.state.input}
 							onChange={this.setInput}
 						/>
-						<button className="border border-black w-32" type="submit">Submit</button>
+						<button className="border border-black w-32" type="submit" style={buttonStyle}>Submit</button>
 					</form>
-					<div className="flex-1">
-						<h2>Challenge Response</h2>
-						<div className="border-2 border-black h-3/4 flex flex-col">
+					<div className="flex-1" style={boxStyle}>
+						<h2 style={headerStyle}>Challenge Response</h2>
+						<div className="h-3/4 flex flex-col">
 							{
 								this.props.status !== undefined && this.props.status !== null
 								? <Results status={this.props.status} output={this.props.challenge.tests.output}/>
