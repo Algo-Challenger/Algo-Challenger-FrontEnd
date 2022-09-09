cd3401ba3c835653ff2187c88050515a660a928b
Adrian Butler Jr <butlerjr123@gmail.com>
2022-09-09 11:04:46 -0500

    added about for me and fixed image sizing

--- src/components/About.js
+++ src/components/About.js
@@ -24,7 +24,7 @@ class About extends React.Component {
 					description="Keelen Fisher, born and raised in Houston Texas and officially a Full Stack Software Developer. Also an undergraduate at the University of Houston Clear Lake for Computer Science. Started with Code Fellows Software Developer program to learn so much more about Advanced Full Stack development in JavaScript to establish projects for communities big and small, while also creating my own."
 				/>
 				<Developer
-					image="1"
+					image={require("../images/IMG_1623 (1).jpeg")}
 					name="Adrian Butler"
 					description="I am Adrian Butler, a Software Engineer from Oklahoma. I first learned how to program a few years ago at a local tech school and from there I developed a huge passion for it and knew that I wanted to pursue a career in programming. After self teaching for a while, I felt I still had a lot to learn and enrolled in Code Fellows. At Code Fellows, I have gained a diverse set of real world skills that allow me to comfortably say I am ready for my first developer job and that I can continue to learn anything I need to be successful."
 				/>
--- src/components/Developer.js
+++ src/components/Developer.js
@@ -10,9 +10,9 @@ class Developer extends React.Component
 					src={this.props.image}
 					// src="https://via.placeholder.com/250"
 					alt="coolest dev in town"
-					className="rounded-full"
+					className="rounded-full h-44 w-44 object-cover"
 				/>
-				<div className="bg-neutral-200 flex-1 mx-10 h-fit px-5 pb-5 pt-3">
+				<div className="flex-1 mx-10 h-fit px-5 pb-5 pt-3">
 					<h1 className="text-center mb-2">{this.props.name}</h1>
 					<p className="">
 						{this.props.description}
